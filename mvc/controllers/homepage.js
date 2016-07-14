4a397897b8ba38dfb1fa6053dcabfc21e837d12c
Troy Coutu <Autre31415@gmail.com>
2016-07-14 16:43:59 -0400

    add last edited and better buttons

--- mvc/controllers/homepage.js
+++ mvc/controllers/homepage.js
@@ -2,6 +2,7 @@ module.exports = function(app) {
   app.route('/').get(function(req, res) {
     var model = require('models/global')(req);
     model.content.pageTitle = 'Home';
+    model.content.pageHeader = 'Home';
     require('models/getBlogPosts')(function (blogPosts) {
       model.blogPosts = blogPosts;
       res.render('homepage', model);
--- mvc/controllers/newPost.js
+++ mvc/controllers/newPost.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
   app.route('/newPost').get(function(req, res) {
     var model = require('models/global')(req);
     model.content.pageTitle = 'New Post';
-    model.content.pageHeader = 'Create Post';
+    model.content.pageHeader = 'Add New Post';
     res.render('newPost', model);
   });
   app.route('/newPost').post(function(req, res) {
